9ad36b53f96e7f83c1cb151e1d61139b4bfd4cb8
Your Name <61065698+zbiernat2000@users.noreply.github.com>
2021-04-15 14:44:26 -0400

    Add variables to store text

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let sproutedText = "For slightly al dente rice :\n"+
+"Combine 1 1/4 cups of rice with 2 cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix." +
+"Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes."+
+"Remove from heat and let stand for 5 minutes. Fluff with a fork and serve."+
+"For softer rice:" +
+"Increase liquid by 1/2 cup and cook time by 5 minutes.";
+
+let whiteText = "Combine 1 cup of rice with 2 cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.";
+
+
