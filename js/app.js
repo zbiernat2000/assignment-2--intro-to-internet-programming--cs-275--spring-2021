fe08f3f3d63d7d99b8199087f6f93549e53a9807
Your Name <61065698+zbiernat2000@users.noreply.github.com>
2021-04-15 15:03:21 -0400

    Add onclick functions to buttons

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
-window.onload = () =>{
-    let sproutedText = "For slightly al dente rice :\n"+
-    "Combine 1 1/4 cups of rice with 2 cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix.\n" +
-    "Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes.\n"+
-    "Remove from heat and let stand for 5 minutes. Fluff with a fork and serve.\n"+
-    "For softer rice:\n" +
-    "Increase liquid by 1/2 cup and cook time by 5 minutes.\n";
+let recipe = document.getElementById('recipe');
+let whiteButton = document.getElementById('white');
+let sproutedButton = document.getElementById('sprouted');
+let whiteText = "Combine 1 cup of rice with 2 cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.";
+let sproutedText = "For slightly al dente rice :\n"+
+"Combine 1 1/4 cups of rice with 2 cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix.\n" +
+"Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes.\n"+
+"Remove from heat and let stand for 5 minutes. Fluff with a fork and serve.\n"+
+"For softer rice:\n" +
+"Increase liquid by 1/2 cup and cook time by 5 minutes.\n";
 
-    let whiteText = "Combine 1 cup of rice with 2 cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.";
 
-    let recipe = document.getElementById('recipe');
-    let whiteButton = document.getElementById('white');
-    let sproutedButton = document.getElementById('sprouted');
-
-}
-
+//Sets text content of recipe
 function setText(string){
     recipe.textContent = string;
 }
 
+whiteButton.onclick = () =>{
+    setText(whiteText);
+}
+
+sproutedButton.onclick = () =>{
+    setText(sproutedText);
+}
