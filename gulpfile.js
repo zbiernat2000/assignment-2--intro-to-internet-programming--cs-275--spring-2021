5e2735b82a20cb7dc4129e3566e8c39fa4614f5e
Your Name <61065698+zbiernat2000@users.noreply.github.com>
2021-04-15 16:50:52 -0400

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,174 @@
+const { src, dest, series, watch } = require(`gulp`);
+const del = require(`del`);
+const babel = require(`gulp-babel`);
+const htmlCompressor = require(`gulp-htmlmin`);
+const htmlValidator = require(`gulp-html`);
+const jsLinter = require(`gulp-eslint`);
+const jsCompressor = require(`gulp-uglify`);
+const imageCompressor = require(`gulp-imagemin`);
+const cache = require(`gulp-cache`);
+const browserSync = require(`browser-sync`);
+const reload = browserSync.reload;
+let browserChoice = `default`;
+
+
+async function firefox () {
+    browserChoice = `firefox`;
+}
+
+async function chrome () {
+    browserChoice = `google chrome`;
+}
+
+async function opera () {
+    browserChoice = `opera`;
+}
+
+async function edge () {
+    browserChoice = `microsoft-edge`;
+}
+
+async function allBrowsers () {
+    browserChoice = [
+        `safari`,
+        `firefox`,
+        `google chrome`,
+        `opera`,
+        `microsoft-edge`
+    ];
+}
+
+let validateHTML = () => {
+    return src([
+        `dev/html/*.html`,
+        `dev/html/**/*.html`])
+        .pipe(htmlValidator());
+};
+
+let compressHTML = () => {
+    return src([`dev/*.html`,`dev/**/*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let transpileJSForDev = () => {
+    return src(`dev/js/*.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/js`));
+};
+
+let transpileJSForProd = () => {
+    return src(`dev/js/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/js`));
+
+};
+
+let lintJS = () => {
+    return src(`dev/js/*.js`)
+        .pipe(jsLinter({
+            parserOptions: {
+                ecmaVersion: 2017,
+                sourceType: `module`
+            },
+            rules: {
+                indent: [2, 4, {SwitchCase: 1}],
+                quotes: [2, `backtick`],
+                semi: [2, `always`],
+                'linebreak-style': [2, `unix`],
+                'max-len': [1, 85, 4]
+            },
+            env: {
+                es6: true,
+                node: true,
+                browser: true
+            },
+            extends: `eslint:recommended`
+        }))
+        .pipe(jsLinter.formatEach(`compact`, process.stderr));
+};
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        port: 5500,
+        reloadDelay: 50,
+        browser: browserChoice,
+        server: {
+            baseDir: [
+                `temp`,
+                `dev`,
+                `dev/html`
+            ]
+        }
+    });
+
+    watch(`dev/js/*.js`,
+        series(lintJS, transpileJSForDev)
+    ).on(`change`, reload);
+
+
+    watch(`dev/html/**/*.html`,
+        series(validateHTML)
+    ).on(`change`, reload);
+
+    watch(`dev/img/**/*`).on(`change`, reload);
+};
+
+async function clean() {
+    let fs = require(`fs`),
+        i,
+        foldersToDelete = [`./temp`, `prod`];
+
+    for (i = 0; i < foldersToDelete.length; i++) {
+        try {
+            fs.accessSync(foldersToDelete[i], fs.F_OK);
+            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
+                ` directory was found and will be deleted.\n`);
+            del(foldersToDelete[i]);
+        } catch (e) {
+            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
+                ` directory does NOT exist or is NOT accessible.\n`);
+        }
+    }
+
+    process.stdout.write(`\n`);
+}
+
+async function listTasks () {
+    let exec = require(`child_process`).exec;
+
+    exec(`gulp --tasks`, function (error, stdout, stderr) {
+        if (null !== error) {
+            process.stdout.write(`An error was likely generated when invoking ` +
+                `the “exec” program in the default task.`);
+        }
+
+        if (`` !== stderr) {
+            process.stdout.write(`Content has been written to the stderr stream ` +
+                `when invoking the “exec” program in the default task.`);
+        }
+
+        process.stdout.write(`\n\tThis default task does ` +
+            `nothing but generate this message. The ` +
+            `available tasks are:\n\n${stdout}`);
+    });
+}
+
+exports.firefox = series(firefox, serve);
+exports.chrome = series(chrome, serve);
+exports.opera = series(opera, serve);
+exports.edge = series(edge, serve);
+exports.allBrowsers = series(allBrowsers, serve);
+exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
+exports.transpileJSForProd = transpileJSForProd;
+exports.lintJS = lintJS;
+exports.build = series(
+    compressHTML,
+    transpileJSForProd
+);
+exports.serve = series(lintJS, transpileJSForDev, validateHTML, serve);
+exports.clean = clean;
+exports.default = listTasks;
